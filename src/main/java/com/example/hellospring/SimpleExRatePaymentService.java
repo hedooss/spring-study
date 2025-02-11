e380f1a431f4e4c9bd634a4804b864c15aa19b42
seohyejoo <seohyejoo@mediazen.co.kr>
2025-02-11 16:45:30 +0900

    refactor : payment service 추상화

---
+++ src/main/java/com/example/hellospring/SimpleExRatePaymentService.java
@@ -0,0 +1,12 @@
+package com.example.hellospring;
+
+import java.io.IOException;
+import java.math.BigDecimal;
+
+public class SimpleExRatePaymentService extends PaymentService{
+    BigDecimal getExRate(String currency) throws IOException {
+        if(currency.equals("USD")) return new BigDecimal("1000");
+
+        throw new IllegalArgumentException("지원되지 않는 통화입니다.");
+    }
+}
